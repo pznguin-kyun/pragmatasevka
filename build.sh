000ad887bff8838cc825f781178c0fcad2e38c22
p3nguin-kun <p3nguinkun@proton.me>
2023-12-03 15:10:57 +0700

    fix script

--- build.sh
+++ build.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 # Pragmatasevka build script
 
-CRE=$(tput setaf 1)
-CYE=$(tput setaf 3)
-CGR=$(tput setaf 2)
-CBL=$(tput setaf 4)
-BLD=$(tput bold)
-CNC=$(tput sgr0)
-
 patch=private-build-plans.toml
 
 greeting(){
-  echo "Welcome to Pragmatasevka build script, $USER!""${BLD}" "${CRE}" "${CNC}"
-  echo "Make sure you have installed the following packages in your system: nodejs, npm, ttfautohint, zip." "${BLD}" "${CRE}" "${CNC}"
+  echo "Welcome to Pragmatasevka build script, $USER!"  
+  echo "Make sure you have installed the following packages in your system: nodejs, npm, ttfautohint, zip."  
   sleep 3
 }
 
@@ -25,6 +18,7 @@ clone(){
   fi
 }
 
+# Make Pragmatasevka
 make(){
   [ -f ./Iosevka/$patch ] && rm -rf ./Iosevka/$patch
   cp $patch ./Iosevka
@@ -37,11 +31,12 @@ make(){
   rm -rf ./Iosevka/dist
 }
 
-done(){
-  echo "Pragmatasevka has been built!" "${BLD}" "${CYE}" "${CNC}"
+# Print done
+done_msg(){
+  echo "Pragmatasevka has been built!"
 }
 
 greeting
 clone
 make
-done
+done_msg
